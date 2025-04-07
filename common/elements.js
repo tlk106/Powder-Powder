b8a919a3d026bc98c1c6e2b01423d4a28abbb702
TLK106 <milenatom25@gmail.com>
2025-04-06 23:05:51 -0400

    Add mass parameter

--- common/elements.js
+++ common/elements.js
@@ -1,12 +1,20 @@
 class Element {
-  constructor(name, type, id, color) {
-    if (typeof name !== "string" || typeof type !== "string" || typeof id !== "number" || typeof color !== "string") {
+  constructor(name, type, id, color, mass) { // Added mass as a parameter
+    if (
+      typeof name !== "string" ||
+      typeof type !== "string" ||
+      typeof id !== "number" ||
+      typeof color !== "string" ||
+      typeof mass !== "number" // Check for 'mass'
+    ) {
       throw new Error("Invalid parameters");
     }
+
     this.name = name;
     this.type = type;
     this.id = id;
     this.color = color;
+    this.mass = mass; // Should be the real-life object mass in grams per cm^3
   }
 
   // Check if the element is of a specific type
@@ -15,10 +23,10 @@ class Element {
   }
 }
 
-const powder = new Element("Powder", "powder", 0, "yellow");
-const water = new Element("Water", "liquid", 1, "blue");
-const oxygen = new Element("Oxygen", "gas", 2, "lightblue");
-const stone = new Element("Stone", "solid", 3, "gray");
+const powder = new Element("Powder", "powder", 0, "yellow", 1);
+const water = new Element("Water", "liquid", 1, "blue", 1);
+const oxygen = new Element("Oxygen", "gas", 2, "lightblue", 0.001429);
+const stone = new Element("Stone", "solid", 3, "gray", 2.5);
 
 const elements = { powder, water, oxygen, stone }; // Group elements into an object
 
