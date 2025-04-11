2bb48b97b97dca3028b8d18d3f4735dd356b1444
TLK106 <milenatom25@gmail.com>
2025-04-11 14:41:36 -0400

    Create temperature points and remove old type system in elements.js

--- common/elements.js
+++ common/elements.js
@@ -1,34 +1,33 @@
 class Element {
-  constructor(name, type, id, color, mass) { // Added mass as a parameter
+  constructor(name, id, color, mass, starttemp, meltingpoint, boilingpoint) {
     if (
       typeof name !== "string" ||
-      typeof type !== "string" ||
       typeof id !== "number" ||
       typeof color !== "string" ||
-      typeof mass !== "number" // Check for 'mass'
+      typeof mass !== "number" ||
+      typeof starttemp !== "number" ||
+      typeof meltingpoint !== "number" ||
+      typeof boilingpoint !== "number"
     ) {
       throw new Error("Invalid parameters");
     }
 
     this.name = name;
-    this.type = type;
     this.id = id;
     this.color = color;
     this.mass = mass; // Should be the real-life object mass in grams per cm^3
-  }
-
-  // Check if the element is of a specific type
-  isType(type) {
-    return this.type === type;
+    this.starttemp = starttemp; // Starting temperature in Celsius
+    this.meltingpoint = meltingpoint; // Melting point in Celsius
+    this.boilingpoint = boilingpoint; // Boiling point in Celsius
   }
 }
 
 // Existing elements with updated masses
-const powder = new Element("Powder", "powder", 0, "yellow", 1.5);
-const water = new Element("Water", "liquid", 1, "blue", 1);
-const oxygen = new Element("Oxygen", "gas", 2, "lightblue", 0.001429);
-const stone = new Element("Stone", "solid", 3, "gray", 2.5);
-const oil = new Element("Oil", "liquid", 4, "orange", 0.9);
+const powder = new Element("Powder", 0, "yellow", 1.5, 21, 1000, 8000);
+const water = new Element("Water", 1, "blue", 1, 21, 0, 100);
+const oxygen = new Element("Oxygen", 2, "lightblue", 0.001429, 21, -218.79, -182.96);
+const stone = new Element("Stone", 3, "gray", 2.5, 21, 1000, 8000);
+const oil = new Element("Oil", 4, "orange", 0.9, 21, -6, 300);
 
 const elements = { powder, water, oxygen, stone, oil }; // Group elements into an object
 
