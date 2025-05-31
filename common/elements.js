9666cfe2bbc63c9b1337de25a598017028e156a3
TLK106 <milenatom25@gmail.com>
2025-05-31 12:30:46 -0400

    Add fire an wood as elements. Add a scroll bar to the element list.

--- common/elements.js
+++ common/elements.js
@@ -1,5 +1,5 @@
 class Element {
-  constructor(name, id, color, mass, starttemp, meltingpoint, boilingpoint, ispowder, conductivity) {
+  constructor(name, id, color, mass, starttemp, meltingpoint, boilingpoint, ispowder, conductivity, isflammable, isfire) {
     if (
       typeof name !== "string" ||
       typeof id !== "number" ||
@@ -9,7 +9,9 @@ class Element {
       typeof meltingpoint !== "number" ||
       typeof boilingpoint !== "number" ||
       typeof ispowder !== "boolean" || // Is powder instead of solid.
-      typeof conductivity !== "number"
+      typeof conductivity !== "number" ||
+      typeof isflammable !== "boolean" ||
+      typeof isfire !== "boolean"
     ) {
       throw new Error("Invalid parameters");
     }
@@ -23,6 +25,8 @@ class Element {
     this.boilingpoint = boilingpoint; // Boiling point in Celsius
     this.ispowder = ispowder; // Is powder instead of solid.
     this.conductivity = conductivity; // Thermal conductivity (arbitrary units)
+    this.isflammable = false; // Default value, can be set later
+    this.isfire = false; // Default value, can be set later
   }
   isSolid() {
     return this.starttemp < this.meltingpoint;
@@ -38,16 +42,18 @@ class Element {
   }
 }
 
-// Existing elements with updated colors as RGB strings and conductivity (arbitrary units)
-const powder = new Element("Powder", 0, "rgb(255, 255, 0)", 1.5, 21, 1000, 8000, true, 0.05);
-const water = new Element("Water", 1, "rgb(30, 99, 190)", 1, 21, 0, 100, false, 0.6);
-const oxygen = new Element("Oxygen", 2, "rgb(173, 216, 230)", 0.001429, 21, -218.79, -182.96, false, 0.02);
-const stone = new Element("Stone", 3, "rgb(128, 128, 128)", 2.5, 21, 1500, 8000, false, 2.0);
-const oil = new Element("Oil", 4, "rgb(255, 165, 0)", 0.9, 21, -6, 300, false, 0.15);
-const ice = new Element("Ice", 5, "rgb(173, 216, 230)", 0.92, -10, 0, 100, false, 2.2);
-const steam = new Element("Steam", 6, "rgb(161, 190, 209)", 0.0006, 300, 0, 100, false, 0.02);
-const lava = new Element("Lava", 7, "rgb(255, 80, 0)", 2.4, 1600, 700, 3000, false, 1.5);
+// Define the elements with their properties
+const powder = new Element("Powder", 0, "rgb(255, 255, 0)", 1.5, 21, 1000, 8000, true, 0.05, false, false);
+const water = new Element("Water", 1, "rgb(30, 99, 190)", 1, 21, 0, 100, false, 0.6, false, false);
+const oxygen = new Element("Oxygen", 2, "rgb(173, 216, 230)", 0.001429, 21, -218.79, -182.96, false, 0.02, true, false);
+const stone = new Element("Stone", 3, "rgb(128, 128, 128)", 2.5, 21, 1500, 8000, false, 2.0, false, false);
+const oil = new Element("Oil", 4, "rgb(255, 165, 0)", 0.9, 21, -6, 300, false, 0.15, true, false);
+const ice = new Element("Ice", 5, "rgb(173, 216, 230)", 0.92, -10, 0, 100, false, 2.2, false, false);
+const steam = new Element("Steam", 6, "rgb(161, 190, 209)", 0.0006, 300, 0, 100, false, 0.02, false, false);
+const lava = new Element("Lava", 7, "rgb(255, 80, 0)", 2.4, 1600, 700, 3000, false, 1.5, false, false);
+const wood = new Element("Wood", 8, "rgb(222, 151, 100)", 0.6, 21, 200, 600, false, 0.1, true, false);
+const fire = new Element("Fire", 9, "rgb(255, 0, 0)", 0.001, 300, 300, 1000, false, 0.01, false, true);
 
-const elements = { powder, water, oxygen, stone, oil, ice, steam, lava };
+const elements = { powder, water, oxygen, stone, oil, ice, steam, lava, wood, fire };
 
 export { elements, Element };
